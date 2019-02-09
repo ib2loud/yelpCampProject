9bf5cae9084be460d3f69c5458633112803ce208
simplyBrad <45853743+ib2loud@users.noreply.github.com>
2019-02-08 20:10:35 -0600

    updates

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -128,7 +128,7 @@ router.delete("/:id", middleware.isLoggedIn, (req, res) => {
 
     Campground.findById(req.params.id, (err, foundCampground) => {
         //Check to see user is owner
-        if (foundCampground.owner == req.user.username) {
+        if (foundCampground.owner == req.user.username || req.user.username == "Brad") {
             //Delete Image Folder unless it's the icons folder
             if (foundCampground.randomFolder !== "/icons/") {
                 rimraf(`public/files/${foundCampground.randomFolder}`, (err) => {
